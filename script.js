8688caf72c02bd43d36c61ca498ebb0ec73aefbb
hd2s <harshd2singh@gmail.com>
2024-01-06 21:48:52 +0530

    variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const singlequotes = 'hello'
+console.log(singlequotes)
+const doublequotes = "Hello!!"
+console.log(doublequotes)
+// no difference between singlequotes and double quotes
+const name = 'hay';
+const backticks = `hi, ${2+2}`;
+console.log(backticks); 
