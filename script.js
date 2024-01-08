9d60725c2413ec0c002fc14b8d4092bc5992e52d
hd2s <harshd2singh@gmail.com>
2024-01-08 14:27:07 +0530

    numbers

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const singlequotes = 'hello'
-console.log(singlequotes)
-const doublequotes = "Hello!!"
-console.log(doublequotes)
-// no difference between singlequotes and double quotes
-const name = 'hay';
-const backticks = `hi, ${2+2}`;
-console.log(backticks); 
+// Number
+
+const whole = 'hello';
+const decimal = 0.333;
+console.log(whole);
+console.log(decimal);
+
+const result = whole/decimal
+console.log(typeof result)
