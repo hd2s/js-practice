c3f6a76494b3f826137f416187bbbd398cf27489
hd2s <harshd2singh@gmail.com>
2024-01-24 15:13:52 +0530

    truthy values

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // truthy and falsy values
 
-if(5/5 == true) {
+if(0) {
     console.log("Truthy");
 }
 else {
     console.log("Falsy");
 }
+
+//falsy values are 0, null, undefined, NaN, false, "" (empty string)
+//truthy values are all values other than falsy values
