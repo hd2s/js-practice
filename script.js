db4c82fd77ca96d8c0a4e207f23bc311786957a1
hd2s <harshd2singh@gmail.com>
2024-01-24 15:04:47 +0530

    if statement is used to check the condition and execute the code if the condition is true.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // if statement
 
 const age = 18;
-if(age >= 18) {
-    console.log("You are an adult!");
+if(age > 18) {
+    console.log("You are an adult, you can vote!");
+}
+else if(age === 18) {
+    console.log("You just turned 18, welcome!");
+}
+else {
+    console.log("You are not an adult, you cannot vote!");
 }
