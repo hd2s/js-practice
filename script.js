07f75290abdf1d0b2f39d8f40722d0deea14a504
hd2s <harshd2singh@gmail.com>
2024-01-24 15:34:50 +0530

    logical operators part 2

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ console.log('' || 0  || NaN || undefined)
 // Output: undefined
 console.log(1 && true && "damn" && 999)
 // Output: 999
+
+var value = 0 ;
+if(!!value )
+{
+        console . log ( "value is truthy" ) ;
+}
+else
+{
+        console . log ( "value is falsy" ) ;
+}
